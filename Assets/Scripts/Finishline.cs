b67000a9ede0da9f07cf69ba92723076d2b95bba
Kiss Tibor <kuczora.tibi@gmail.com>
2026-05-26 15:28:54 +0200

    fix collision issue

--- Assets/Scripts/Finishline.cs
+++ Assets/Scripts/Finishline.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class Finishline : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("Collision detected.");
+        int layerindex = LayerMask.NameToLayer("Player");
+        if (other.gameObject.layer == layerindex) {
+            Debug.Log("Player has reached the finish line!");
+        }
     }
 }

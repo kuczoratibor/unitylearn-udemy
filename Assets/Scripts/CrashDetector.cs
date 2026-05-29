d736c371e31b022b6eede0d8c7a80ae572ce1c41
Kiss Tibor <kuczora.tibi@gmail.com>
2026-05-29 15:14:59 +0200

    Course 3 section 44/59

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,13 +4,15 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float restartDelay = 1f;
+    [SerializeField] ParticleSystem crashParticles;
     void OnTriggerEnter2D(Collider2D other) {
         int layerindex = LayerMask.NameToLayer("Floor");
         if (other.gameObject.layer == layerindex) {
-                Invoke("ReloadScene", restartDelay);
+            crashParticles.Play();
+            Invoke("ReloadScene", restartDelay);
         }
     }
-        void ReloadScene() {
+    void ReloadScene() {
         SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/Finishline.cs
+++ Assets/Scripts/Finishline.cs
@@ -4,9 +4,11 @@ using UnityEngine.SceneManagement;
 public class Finishline : MonoBehaviour
 {
     [SerializeField] float restartDelay = 1f;
+    [SerializeField] ParticleSystem winParticles;
     void OnTriggerEnter2D(Collider2D other) {
         int layerindex = LayerMask.NameToLayer("Player");
         if (other.gameObject.layer == layerindex) {
+            winParticles.Play();
             Invoke("ReloadScene", restartDelay);
         }
     }

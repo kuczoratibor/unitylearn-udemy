3e565ff5eade7362fd8336247cbf48a307f75a78
Kiss Tibor <kuczora.tibi@gmail.com>
2026-06-25 15:10:57 +0200

    Course 3 section 46/59

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,23 +4,39 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float baseSpeed = 11f;
+    [SerializeField] float boostSpeed = 15f;
     InputAction moveAction;
     Vector2 MoveVector;
     Rigidbody2D rb;
+    SurfaceEffector2D surfaceEffector2D;
     void Start() {
         moveAction = InputSystem.actions.FindAction("Move");
         rb = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindFirstObjectByType<SurfaceEffector2D>();
     }
 
     void Update()
     {
-        
+        RotatePlayer();
+        BoostPlayer();
+
+    }
+    void RotatePlayer() {
         MoveVector = moveAction.ReadValue<Vector2>();
         if (MoveVector.x < 0) {
             rb.AddTorque(torqueAmount);
         }
         else if (MoveVector.x > 0) {
             rb.AddTorque(-torqueAmount);
+        }
+    }
+    void BoostPlayer() {
+        if (MoveVector.y > 0) {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else {
+            surfaceEffector2D.speed = baseSpeed;
         }
     }
 }

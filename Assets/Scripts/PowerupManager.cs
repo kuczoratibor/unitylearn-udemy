17a748263ebf51381ef7448715b3a159059ffb0a
Kiss Tibor <kuczora.tibi@gmail.com>
2026-07-28 14:36:58 +0200

    Course 3 section 54/59 alternative method

--- Assets/Scripts/PowerupManager.cs
+++ Assets/Scripts/PowerupManager.cs
@@ -4,16 +4,19 @@ public class PowerupManager : MonoBehaviour
 {
 [SerializeField] PowerupSO powerup;
 PlayerController player;
+SpriteRenderer spriteRenderer;
     void Start()
     {
         player = FindFirstObjectByType<PlayerController>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         int layerindex = LayerMask.NameToLayer("Player");
 
-        if (other.gameObject.layer == layerindex) {
+        if (other.gameObject.layer == layerindex && spriteRenderer.enabled) {
+            spriteRenderer.enabled = false;
             Debug.Log("Powerup Triggered");
             player.ActivatePowerup(powerup);
         }
